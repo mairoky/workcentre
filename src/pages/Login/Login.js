926e899c0d52291e88d47f43e16439ac947d916a
Aminul Islam Rocky <mairoky@outlook.com>
2022-12-26 11:02:00 +0600

    login & signup page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { FaGoogle } from 'react-icons/fa';
+import LoginImage from '../../assets/images/process-01.png';
+import './Login.css';
 
 const Login = () => {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    // Handle Login Form
+    const handleLogin = (data) => {
+
+    }
+    // Handle Google Login
+    const handleGoogleLogIn = () => {
+
+    }
+
     return (
-        <div>
-            Login
-        </div>
+        <section className='login'>
+            <div className="container">
+                <div className="row g-3 align-items-lg-center">
+                    <div className="col-lg-6 col-md-7 col-12">
+                        <div className="card border-0 py-4">
+                            <form onSubmit={handleSubmit(handleLogin)}>
+                                <div className="card-body">
+                                    <div className="text-center">
+                                        <h3>Login</h3>
+                                    </div>
+                                    <hr />
+                                    <div className="form-group mt-2">
+                                        <label htmlFor="email">Email</label>
+                                        <input {...register('email', { required: 'Email is Required.' })} type="email" id="email" className="form-control" />
+                                        {errors.email && <p className='text-danger m-0'>{errors.email.message}</p>}
+                                    </div>
+                                    <div className="form-group mt-2">
+                                        <label htmlFor="password">Password</label>
+                                        <input {...register('password', { required: 'Password is Required.' })} type="password" id="password" className="form-control" />
+                                        {errors.password && <p className='text-danger m-0'>{errors.password.message}</p>}
+                                    </div>
+                                    <div className="text-center">
+                                        <button type="submit" className="w-50 btn btn-general mt-3">Login</button>
+                                        <Link to="/register" className="d-block mt-2">Don't have Account? <strong>Register Now</strong></Link>
+                                    </div>
+                                    <hr />
+                                    <div className="text-center">
+                                        <Link onClick={handleGoogleLogIn} className="w-50 btn btn-general"><FaGoogle /> Login with Google</Link>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-md-5 col-12">
+                        <div className="login-image">
+                            <img className='img-fluid' src={LoginImage} alt="work centre" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,10 +1,63 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import RegisterImage from '../../assets/images/process-02.png';
+import './Register.css';
 
 const Register = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    // Handle Register Form
+    const handleRegister = (data) => {
+
+    }
     return (
-        <div>
-            Register
-        </div>
+        <section className='register'>
+            <div className="container">
+                <div className="row g-3 align-items-lg-center">
+                    <div className="col-lg-6 col-md-5 col-12">
+                        <div className="register-image">
+                            <img className='img-fluid' src={RegisterImage} alt="Work Centre" />
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-md-7 col-12">
+                        <div className="card border-0 py-4">
+                            <form onSubmit={handleSubmit(handleRegister)}>
+                                <div className="card-body">
+                                    <div className="text-center">
+                                        <h3>Create Account</h3>
+                                    </div>
+                                    <hr />
+                                    <div className="form-group mt-2">
+                                        <label htmlFor="name">Name</label>
+                                        <input {...register('name', {
+                                            required: "Name is Required."
+                                        })} type="text" id="name" className="form-control" />
+                                        {errors.name && <p className='text-danger m-0'>{errors.name.message}</p>}
+                                    </div>
+                                    <div className="form-group mt-2">
+                                        <label htmlFor="email">Email</label>
+                                        <input {...register('email')} type="email" id="email" className="form-control" />
+                                        {errors.email && <p className='text-danger m-0'>{errors.email.message}</p>}
+                                    </div>
+                                    <div className="form-group mt-2">
+                                        <label htmlFor="password">Password</label>
+                                        <input {...register('password')} type="password" id="password" className="form-control" required />
+                                    </div>
+                                    <div className="text-center">
+                                        <button type="submit" className="w-50 btn btn-general mt-3">Register</button>
+                                        <Link to="/login" className="d-block mt-2">Already have an Account? <strong>Login Now</strong></Link>
+                                    </div>
+                                    {/* <hr />
+                                    <div className="text-center">
+                                        <Link onClick={handleGoogleLogIn} className="w-50 btn btn-outline-dark"><FaGoogle /> Signup with Google</Link>
+                                    </div> */}
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
