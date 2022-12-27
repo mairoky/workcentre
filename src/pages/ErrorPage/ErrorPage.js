6b7225b4cd066eb2f8283a2bb36dbd3eb7b8c00b
Aminul Islam Rocky <mairoky@outlook.com>
2022-12-27 10:28:58 +0600

    error page added

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './ErrorPage.css';
 
 const ErrorPage = () => {
     return (
-        <div>
-            ErrorPage
-        </div>
+        <section className='error-page'>
+            <div className="h-100 d-flex justify-content-center align-items-center error-page-container">
+                <div className="content-box text-center">
+                    <h1 className='m-0'>404</h1>
+                    <h3 className='m-0'>Look like you're lost!</h3>
+                    <p>The page you are looking for not available!</p>
+                    <Link to="/" className="btn btn-general">Back to Home</Link>
+                </div>
+            </div>
+        </section>
     );
 };
 
